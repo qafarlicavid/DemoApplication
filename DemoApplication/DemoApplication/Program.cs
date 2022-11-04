8ccb2696c9cb4e651da5fcfe3a70baef940773a1
qafarlicavid <javidig@code.edu.az>
2022-11-04 21:02:38 +0400

    static files (pustok) uploaded

---
+++ DemoApplication/DemoApplication/Program.cs
@@ -0,0 +1,18 @@
+namespace DemoApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddMvc();
+            var app = builder.Build();
+            app.UseStaticFiles();
+
+            app.MapControllerRoute(name: "default", pattern: "{controller}/{action}");
+
+
+            app.Run();
+        }
+    }
+}
